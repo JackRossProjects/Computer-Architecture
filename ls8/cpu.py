eee9635dcdd592c005b2f53c17cc1981baf0f4d6
User <jackrosscoding@gmail.com>
2020-08-18 13:13:44 -0600

    all but hardcoding finished

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,31 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+SP = 7
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.branch_table = {
+            LDI: self.handle_ldi,
+            PRN: self.handle_prn,
+            MUL: self.handle_mul,
+            HLT: self.handle_hlt,
+            PUSH: self.handle_push,
+            POP: self.handle_pop
+        }
+
 
     def load(self):
         """Load a program into memory."""
@@ -25,18 +44,56 @@ class CPU:
             0b00000000,
             0b00000001, # HLT
         ]
-
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(instruction, address)
             address += 1
+
+    def handle_ldi(self, reg, val):
+        self.reg[reg] = val
+
+    def handle_prn(self, reg, *args):
+        print(self.reg[reg])
+
+    def handle_mul(self, reg_a, reg_b):
+        self.reg[reg_a] *= self.reg[reg_b]
+
+    def handle_hlt(self, *args):
+        exit()
+
+    def handle_push(self, reg, *args):
+        # Increment stack pointer
+        self.reg[SP] += 1
+
+        self.ram_write(self.reg[reg], self.reg[SP])
+
+    def handle_pop(self, reg, *args):
+        if self.reg[SP] == 0xF4:
+            return None
+        val = self.ram_read(self.reg[SP])
+        self.reg[SP] -= 1
+        self.reg[reg] = val 
+
+    def ram_read(self, MAR):
+        # Returns a memory address register in RAM
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        # Sets the the ram value at that memory address
+        # register to the memory data register
+        self.ram[MAR] = MDR
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        # If we're given an ADD operation
         if op == "ADD":
+            # Increment the first value by the second
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # If we're given a MUL operation (multiplication)
+        elif op == "MUL":
+            # Multiply those the first value by the second
+            self.reg[reg_a] *= self.reg[reg_b]
+        # Otherwise the operation is unsupported
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -45,7 +102,6 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -54,12 +110,20 @@ class CPU:
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
-
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
-        print()
-
     def run(self):
         """Run the CPU."""
-        pass
+        self.pc = 0
+        halted = False
+
+        while not halted:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+            op_count = IR >> 6
+
+            self.branch_table[IR](operand_a, operand_b)
+
+            self.pc += op_count + 1
